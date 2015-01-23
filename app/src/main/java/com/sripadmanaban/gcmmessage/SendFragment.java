09c67adabd2fe65a822230e3328048b6e0456d1b
Sripadmanaban Subramanian <sripadmanabans@gmail.com>
2015-01-23 15:56:14 -0600

    Made some warning go away

--- app/src/main/java/com/sripadmanaban/gcmmessage/SendFragment.java
+++ app/src/main/java/com/sripadmanaban/gcmmessage/SendFragment.java
@@ -6,7 +6,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.nfc.Tag;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -30,7 +29,6 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.json.Json;
 import javax.json.JsonObject;
